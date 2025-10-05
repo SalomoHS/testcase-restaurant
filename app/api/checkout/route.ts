93eaf66a9c52e8eee6c42df67098a1bb56e56424
SalomoHS <isalomohendriyan@gmail.com>
2025-10-05 12:39:34 +0700

    pre-final-2

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,12 +2,6 @@ import { type NextRequest, NextResponse } from "next/server"
 import { createPendingOrder } from "@/lib/mock-db"
 import Midtrans from "midtrans-client"
 
-// let core = new Midtrans.CoreApi({
-//         isProduction : false,
-//         serverKey : process.env.MIDTRANS_SERVER_KEY,
-//         clientKey : process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY
-//     });
-
 let snap = new Midtrans.Snap({
         isProduction : false,
         serverKey : process.env.MIDTRANS_SERVER_KEY,
@@ -20,40 +14,24 @@ export async function POST(req: NextRequest) {
   if (!Array.isArray(items) || items.length === 0) {
     return new NextResponse("No items", { status: 400 })
   }
-  // let parameter = {
-  //           "payment_type": "gopay",
-  //           "transaction_details": {
-  //               "gross_amount": 20000,
-  //               "order_id": ~~(Math.random() * 100)+1,
-  //           }
-  //       };
+
   const order = createPendingOrder(items)
   let parameter = {
     "transaction_details": {
         "order_id": order.id,
         "gross_amount": 10000 * 2
     },
-    "item_details": [{
-        "name": "Test Item",
-        "price": 10000,
-        "quantity": 1
-    },{
-        "name": "Test Item 2",
-        "price": 10000,
-        "quantity": 1
-    }]
+    "item_details": items.map((i) => ({
+            name: i.name,
+            price: i.price,
+            quantity: i.quantity,
+        }))
 };
 
-//     // charge transaction
-//     // const chargeResponse = await core.charge(parameter)
-//     // console.log(chargeResponse);
    
   const token = await snap.createTransaction(parameter)
   console.log(token);
 
-  
-  const method = paymentMethod || "bca_va"
-  const vaNumber = `3901${Math.floor(100000000 + Math.random() * 900000000)}`
   return NextResponse.json({
     token: token.token, order_id: order.id
   })
